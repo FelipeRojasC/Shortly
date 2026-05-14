bb69c79cd2279602166bfca518076a4ae8b0a3d1
FelipeRojasC <felipe.rojas02@alumnos.ucn.cl>
2026-05-14 11:45:54 -0400

    feat: updated DbInitializer to include sample links

--- Infrastructure/Seed/DbInitializer.cs
+++ Infrastructure/Seed/DbInitializer.cs
@@ -13,20 +13,28 @@ public class DbInitializer
         log.LogInformation("Applying migrations...");
         await context.Database.MigrateAsync();
 
-        if (await context.Users.AnyAsync())
-        {
-            return;
-        }
+        if (await context.Users.AnyAsync()) return;
 
-        log.LogInformation("Seeding initial users...");
-        var users = new User[]
-        {
-            new User("carl.johnson@test.com", "password321"),
-            new User("will.smith@test.com", "password654")
-        };
+        log.LogInformation("Seeding initial data...");
 
-        await context.Users.AddRangeAsync(users);
+        // 1. Crear usuarios
+        var user1 = new User("carl.johnson@test.com", "password321");
+        var user2 = new User("will.smith@test.com", "password654");
+
+        await context.Users.AddRangeAsync(user1, user2);
         await context.SaveChangesAsync();
         log.LogInformation("Database seeded successfully.");
+
+        var links = new Link[]
+    {
+        new Link("https://www.google.com", "goog", user1.Id),
+        new Link("https://github.com/FelipeRojasC", "my-git", user1.Id),
+        new Link("https://www.ucn.cl", "ucn", user2.Id)
+    };
+
+        await context.Links.AddRangeAsync(links);
+        await context.SaveChangesAsync();
+
+        log.LogInformation("Database seeded with users and links.");
     }
 }
